50f74617578d4495f4e8654118e9d0328322a83b
Daniel Didah <didahdaniel@gmail.com>
2020-02-24 13:24:32 +0300

    added sender for outgoing messages

---
+++ src/main/java/com/didahdx/app/Sender.java
@@ -0,0 +1,35 @@
+package com.didahdx.app;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import sun.rmi.runtime.Log;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+   public static void main(String [] args){
+      ConnectionFactory connectionFactory =new ConnectionFactory();
+String message="test Message";
+      try {
+         Connection connection = connectionFactory.newConnection();
+         Channel channel = connection.createChannel();
+         channel.queueDeclare("testing-mq", false, false,
+                 false, null);
+         channel.basicPublish("","testing-mq",false,
+                 null,message.getBytes());
+
+        System.out.println("RabbitMQ message sent!!!");
+
+      } catch (IOException e) {
+         e.printStackTrace();
+
+      } catch (TimeoutException e) {
+         e.printStackTrace();
+      }catch (Exception e){
+         e.printStackTrace();
+      }
+   }
+}
