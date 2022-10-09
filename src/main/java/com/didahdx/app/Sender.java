8a14f5d03d7ff8943ccaf4a7678fb7bbdaa557ef
Daniel Didah <didahdaniel@gmail.com>
2022-10-09 18:31:34 +0300

    minor update

--- src/main/java/com/didahdx/app/Sender.java
+++ src/main/java/com/didahdx/app/Sender.java
@@ -3,7 +3,7 @@ package com.didahdx.app;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import sun.rmi.runtime.Log;
+
 
 import java.io.IOException;
 import java.util.Date;
@@ -12,7 +12,13 @@ import java.util.concurrent.TimeoutException;
 public class Sender {
 
    public static void main(String [] args){
+       String host="sparrow.rmq.cloudamqp.com";
+       String username="alnniacs";
+       String password="7ZxupMmtsfqXFIe9WFQeUrcnQI7HhXu4";
       ConnectionFactory connectionFactory =new ConnectionFactory();
+      connectionFactory.setHost(host);
+      connectionFactory.setUsername(username);
+      connectionFactory.setPassword(password);
 String message=": \n"+ new Date().toString()+" \t test Message \n";
       try {
          Connection connection = connectionFactory.newConnection();
